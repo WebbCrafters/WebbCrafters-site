56ec2245170b87459820b20b13e7a66f23c2ef7e
Mateus Freitas <162519025+WebbCrafters@users.noreply.github.com>
2024-03-15 20:20:14 -0300

    4° Atualização
    - Adição do menu na versão portátil

---
+++ script.js
@@ -0,0 +1,33 @@
+function expandir1() {
+    var p = document.getElementById("paragrafoRetratil1");
+    p.classList.toggle("expandir");
+    var seta = document.getElementById("seta1");
+    seta.classList.toggle("setaParaCima");
+}
+function expandir2() {
+    var p = document.getElementById("paragrafoRetratil2");
+    p.classList.toggle("expandir");
+    var seta = document.getElementById("seta2");
+    seta.classList.toggle("setaParaCima");
+}
+function expandir3() {
+    var p = document.getElementById("paragrafoRetratil3");
+    p.classList.toggle("expandir");
+    var seta = document.getElementById("seta3");
+    seta.classList.toggle("setaParaCima");
+}
+
+let menuAberto = false;
+
+function expandirMenu() {
+    const menu = document.querySelector('.espacoMenu');
+    const iconMenu = document.querySelector('#iconMenuDois');
+
+    if (!menuAberto) {
+        menu.classList.add('aberto'); // Adiciona a classe 'aberto' para mostrar o menu
+    } else {
+        menu.classList.remove('aberto'); // Remove a classe 'aberto' para esconder o menu
+    }
+
+    menuAberto = !menuAberto; // Inverte o estado do menu
+}
